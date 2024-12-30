64acd0b61cae8dccce81a9d7cf7b4503466fc3e2
Jake Runyan <runyanjake@gmail.com>
2024-12-29 20:17:26 -1000

    Style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,18 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Jake Runyan</p>
+            <div className="footer-links left">
+                <a href="https://github.com/runyanjake/jakesphotos" rel="noopener noreferrer">GitHub</a>
+                <a href="https://www.instagram.com/jakerunyanphotography" rel="noopener noreferrer">Instagram</a>
+            </div>
+            <div className="footer-title">
+                <p>&copy; {new Date().getFullYear()} Jake Runyan</p>
+            </div>
+            <div className="footer-links right">
+                <a href="/">Home</a>
+                <a href="/contact">Contact</a>
+                <a href="/about">About</a>
+            </div>
         </footer>
     );
 };
